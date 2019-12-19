cc954a1cfd57530c998362ffc8ebbba38bcf1c40
常铖 <1326881231@qq.com>
2019-12-19 23:00:13 +0800

    example

---
+++ Solution.java
@@ -0,0 +1,14 @@
+package 数据结构.二叉树;
+
+public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null)
+            return t2;
+        if(t2 == null)
+            return t1;
+        t1.value += t2.value;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
